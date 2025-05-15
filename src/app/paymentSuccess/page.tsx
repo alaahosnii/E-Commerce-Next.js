2b82314452fbb3cedbbc3e2b07e2752335594d3c
alaahosnii <alaahosnii1000@gmail.com>
2025-05-15 03:21:50 +0300

    Update payment success image import to use a direct path

--- src/app/paymentSuccess/page.tsx
+++ src/app/paymentSuccess/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import paymentSuccess from "@/assets/payment_success.png";
 import Image from 'next/image';
 import PaymentSuccessContent from '../_components/PaymentSuccessContent/PaymentSuccessContent';
 function PaymentSuccess() {
   return (
     <div className='d-flex flex-column justify-content-center align-items-center' style={{ height: "80vh" }}>
-      <Image src={paymentSuccess} alt="payment success" height={300} width={300} />
+      <Image src= "/assets/payment_success.png" alt="payment success" height={300} width={300} />
       <PaymentSuccessContent />
     </div>
   )
